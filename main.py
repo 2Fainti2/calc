34e71a2ccaa018c6a38de0a3a776ba453edbb920
fainti22 <123232037+2Fainti2@users.noreply.github.com>
2023-02-04 08:02:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print("register")
+name = str(input('Write your name: '))
+if isalpha()  == False:
+try:
+     
+        a = int(input('A: '))
+        sign = input('Sign: ')
+        b = int(input('B: '))
+        if sign == '/' and b == 0:
+                raise ZeroDivisionError
+                if sign != '+' and sign != '-' and sign!='*' and sign!='/':
+                        raise Warning
+                elif sign == '+':
+                        print(f"{a} + {b} = {a+b}")
+                elif sign == '-':
+                        print(f"{a} - {b} = {a-b}")
+                elif sign == '*':
+                        print(f"{a} * {b} = {a*b}")
+                elif sign == '/':
+                        print(f"{a} / {b} = {a/b}")        
+except ValueError:
+        print('You need to input digits!')
+except ZeroDivisionError:
+        print('Go to school')
+except Warning:
+        print('Unknown sign')
+except:
+        print('Error!')
